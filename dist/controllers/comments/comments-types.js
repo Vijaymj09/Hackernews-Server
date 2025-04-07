d99eb293cfd44c47432b959ddba9317b80d0d56c
Vijaymj09 <karthimj09@gmail.com>
2025-04-07 09:26:32 +0530

    refactor: remove unused files and clean up the dist directory

--- dist/controllers/comments/comments-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommentError = void 0;
-var CommentError;
-(function (CommentError) {
-    CommentError["NOT_FOUND"] = "NOT_FOUND";
-    CommentError["UNAUTHORIZED"] = "UNAUTHORIZED";
-    CommentError["BAD_REQUEST"] = "BAD_REQUEST";
-})(CommentError || (exports.CommentError = CommentError = {}));
--- dist/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = exports.jwtSecretKey = void 0;
-// Example environment configuration
-exports.jwtSecretKey = process.env.JWT_SECRET_KEY || process.exit(1);
-exports.env = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || 3000,
-};
